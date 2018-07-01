42f0b9d84adad922b55ce9e39d044591c1185d9b
Ezequiel <eze.gimenez.98@gmail.com>
2018-07-01 15:33:44 -0300

    Eliminado parte innecesaria del proyeceto

--- app/src/main/java/com/visoft/jobfinder/MainActivity.java
+++ app/src/main/java/com/visoft/jobfinder/MainActivity.java
@@ -1,18 +1,39 @@
 package com.visoft.jobfinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference databaseReference;
-    private int userIdCount = 0;
-
+    private Button signInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    /**
+     * If user is signed in it will updateUI accordingly
+     */
+    public void isLoggedIn() {
+        FirebaseUser acc = mAuth.getCurrentUser();
+        //updateUI(acc);
+    }
+
 }
