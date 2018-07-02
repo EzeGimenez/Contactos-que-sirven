55db129824c08949e2e31ddc002655894c8ab9b7
Ezequiel <eze.gimenez.98@gmail.com>
2018-07-02 14:52:27 -0300

    Añadida toolbar

--- app/src/main/java/com/visoft/jobfinder/MainActivity.java
+++ app/src/main/java/com/visoft/jobfinder/MainActivity.java
@@ -2,9 +2,15 @@ package com.visoft.jobfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,6 +18,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
     private Button signInButton;
     private FirebaseAuth mAuth;
+    private TextView tvUsername;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +34,63 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Inicializacion de variables
+        tvUsername = findViewById(R.id.tvLoggedInUsername);
+
+
+        //Creacion de toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        isLoggedIn();
     }
 
     /**
      * If user is signed in it will updateUI accordingly
      */
-    public void isLoggedIn() {
+    private void isLoggedIn() {
         FirebaseUser acc = mAuth.getCurrentUser();
-        //updateUI(acc);
+        updateUI(acc);
     }
 
+    /**
+     * Actualiza la interfaz de acuerdo si el usuario ya está registrado
+     * @param user firebase user, puede ser null
+     */
+    private void updateUI(@Nullable FirebaseUser user){
+        if(user != null){ // esta iniciado sesion
+            tvUsername.setText("Bienvenido: " + user.getDisplayName());
+            signInButton.setVisibility(View.INVISIBLE);
+        }else{ // no esta iniciado sesion
+            tvUsername.setVisibility(View.INVISIBLE);
+            signInButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Oyente de la toolbar
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.boton1:
+                Toast.makeText(this, "Bla", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
 }
