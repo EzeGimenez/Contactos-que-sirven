c225fdbd685169c4ae0f3636ce03c5b4fd86c867
Ezequiel <eze.gimenez.98@gmail.com>
2018-07-08 14:54:50 -0300

    Finished Pro User Config

---
+++ app/src/main/java/com/visoft/jobfinder/mainpagefragments/MainPageFragment.java
@@ -0,0 +1,20 @@
+package com.visoft.jobfinder.mainpagefragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.visoft.jobfinder.R;
+
+public class MainPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_page, container, false);
+    }
+
+}
